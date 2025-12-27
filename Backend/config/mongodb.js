f71db645051b8d48a0ffd673f1f43dee77606657
Anshika Verma <146081048+A-verma267@users.noreply.github.com>
2025-12-27 23:46:25 +0530

    ready to go

--- Backend/config/mongodb.js
+++ Backend/config/mongodb.js
@@ -7,5 +7,6 @@ const connectDB = async() =>{
 
     await mongoose.connect(`${process.env.MONGO_URL}/e-commerce`)
 }
-
+// 106.192.199.132 Anshika1@2
+// mongodb+srv://Anshika:Anshika1@2@cluster0.zdb5a4z.mongodb.net/?appName=Cluster0
 export default connectDB;
